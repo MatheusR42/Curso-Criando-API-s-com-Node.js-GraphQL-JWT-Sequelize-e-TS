ad4846e407a3ea14bd6c9c1852de2a771f14ac37
MatheusR42 <matheus.araujo.r42@gmail.com>
2019-09-12 20:30:43 -0300

    47. Post schema: types, queries e mutations

--- graphql-node-api/src/graphql/mutation.ts
+++ graphql-node-api/src/graphql/mutation.ts
@@ -1,7 +1,9 @@
+import { postMutations } from './resources/post/post.schema';
 import { UserMutations } from './resources/users/users.schema'
 
 const Mutation = `
     type Mutation {
+        ${postMutations}
         ${UserMutations}   
     }
 `;
--- graphql-node-api/src/graphql/query.ts
+++ graphql-node-api/src/graphql/query.ts
@@ -1,7 +1,9 @@
+import { postQueries } from './resources/post/post.schema';
 import { UserQueries } from './resources/users/users.schema'
 
 const Query = `
     type Query {
+        ${postQueries}
         ${UserQueries}
     }
 `;
