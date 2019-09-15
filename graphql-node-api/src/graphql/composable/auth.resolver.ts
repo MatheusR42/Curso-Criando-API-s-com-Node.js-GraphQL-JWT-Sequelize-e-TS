87bac7c96b01a3cabe391d4ac6a121bc368a7c45
MatheusR42 <matheus.araujo.r42@gmail.com>
2019-09-15 01:00:43 -0300

    80. User: evitando colisão de nomes

--- graphql-node-api/src/graphql/composable/auth.resolver.ts
+++ graphql-node-api/src/graphql/composable/auth.resolver.ts
@@ -6,7 +6,7 @@ export const authResolver: ComposableResolver<any, ResolverContext> = (
   resolver: GraphQLFieldResolver<any, ResolverContext>
 ): GraphQLFieldResolver<any, ResolverContext> => {
   return (parent, args, ctx: ResolverContext, info) => {
-    if (ctx.user || ctx.authorization) {
+    if (ctx.authUser || ctx.authorization) {
       return resolver(parent, args, ctx, info);
     }
 
