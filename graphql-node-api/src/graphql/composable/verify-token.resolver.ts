7872a43bd12325fb3e7ed606371ec123a9a90cdb
MatheusR42 <matheus.araujo.r42@gmail.com>
2019-09-15 00:34:32 -0300

    77. Composable: verifyTokenResolver

---
+++ graphql-node-api/src/graphql/composable/verify-token.resolver.ts
@@ -0,0 +1,20 @@
+import { GraphQLFieldResolver } from "graphql";
+import * as jwt from 'jsonwebtoken';
+import { ComposableResolver } from "./composable.resolver";
+import { ResolverContext } from "../../interfaces/ResolverContextInterface";
+
+export const verifyTokenResolver: ComposableResolver<any, ResolverContext> = (
+  resolver: GraphQLFieldResolver<any, ResolverContext>
+): GraphQLFieldResolver<any, ResolverContext> => {
+  return (parent, args, ctx: ResolverContext, info) => {
+    const token: string = ctx.authorization ? ctx.authorization.split(' ')[1] : null;
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, _decoded) => {
+        if (!err) {
+            return resolver(parent, args, ctx, info);
+        }
+
+        throw new Error(`${err.name}: ${err.message}`)
+    })
+  }
+};
